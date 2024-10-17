c827706fb07259afa4dc9ab1f56896e64f6fe30b
silgenius <olutademartin@gmail.com>
2024-10-17 14:07:13 +0000

    Implement solution to Task 2

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- a SQL script that creates a table users
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
