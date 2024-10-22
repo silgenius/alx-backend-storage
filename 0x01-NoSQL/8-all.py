3515600fef4ed92b5f1b09b66f8b2f438b09a29d
silgenius <olutademartin@gmail.com>
2024-10-22 06:20:13 -0700

    Fox requirement error

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,8 +4,11 @@
  a Python function that lists all documents in a collection
 """
 
-
 def list_all(mongo_collection):
+    """
+    return the lists all documents in a collection
+    """
+
     collection = list(mongo_collection.find())
     if not collection:
         return []
