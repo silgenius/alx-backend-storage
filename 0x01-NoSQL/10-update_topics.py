713e288034311bc68ac044359ea921a745fe05d6
silgenius <olutademartin@gmail.com>
2024-10-22 06:49:22 -0700

    Fix requirement error

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,6 +7,11 @@ school document based on the name
 
 
 def update_topics(mongo_collection, name, topics):
+    """
+    function that changes all topics of a
+    school document based on the name
+    """
+
     mongo_collection.update_one(
             {"name": name},
             {'$set': {"topics": topics}}
