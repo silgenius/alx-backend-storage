d780dd7c7551f46cf09b267e9b0f3064ca89eddc
silgenius <olutademartin@gmail.com>
2024-10-22 06:55:54 -0700

    Refactor coe to pass all unit test

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -12,7 +12,7 @@ def update_topics(mongo_collection, name, topics):
     school document based on the name
     """
 
-    mongo_collection.update_one(
+    mongo_collection.update_many(
             {"name": name},
             {'$set': {"topics": topics}}
     )
